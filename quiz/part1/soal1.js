4a7e5266727549b72f010ceb48470380b2638595
dwinainggolan <dwigolan@gmail.com>
2025-04-01 22:48:22 +0700

    submit tugas pertama

---
+++ quiz/part1/soal1.js
@@ -0,0 +1,19 @@
+
+let nama = "Dwi";  // Ganti dengan nama yang diinginkan
+let peran = "Ksatria";  // Ganti dengan peran: "Ksatria", "Tabib", atau "Penyihir"
+
+if (nama === "") {
+  console.log("Nama wajib diisi");
+} else if (peran === "") {
+  console.log("Pilih Peranmu untuk memulai game");
+} else {
+  if (peran === "Ksatria") {
+    console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
+  } else if (peran === "Tabib") {
+    console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka`);
+  } else if (peran === "Penyihir") {
+    console.log(`Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
+  } else {
+    console.log("Tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada");
+  }
+}
