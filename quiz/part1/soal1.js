70994869509b909c0f4fe26869148e47b94c8a3b
dwinainggolan <dwigolan@gmail.com>
2025-04-03 09:35:30 +0700

    submit tugas kedua

--- quiz/part1/soal1.js
+++ quiz/part1/soal1.js
@@ -8,7 +8,7 @@ if (nama === "") {
   console.log("Pilih Peranmu untuk memulai game");
 } else {
   if (peran === "Ksatria") {
-    console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
+    console.log(`Halo Ksatria ${gnama}, kamu dapat menyerang dengan senjatamu!`);
   } else if (peran === "Tabib") {
     console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka`);
   } else if (peran === "Penyihir") {
---
+++ quiz/part1/soal2.js
@@ -0,0 +1,48 @@
+let bulan = "";
+let tanggal = 3;
+let tahun = 2005;
+
+bulan = 1;
+
+switch (bulan) {
+    case 1:
+        bulan = "Januari";
+        break;
+    case 2:
+        bulan = "Februari";
+        break;
+    case 3:
+        bulan = "Maret";
+        break;
+    case 4:
+        bulan = "April";
+        break;
+    case 5:
+        bulan = "Mei";
+        break;
+    case 6:
+        bulan = "Juni";
+        break;
+    case 7:
+        bulan = "Juli";
+        break;
+    case 8:
+        bulan = "Agustus";
+        break;
+    case 9:
+        bulan = "September";
+        break;
+    case 10:
+        bulan = "Oktober";
+        break;
+    case 11:
+        bulan = "November";
+        break;
+    case 12:
+        bulan = "Desember";
+        break;
+    default:
+        bulan = "Bulan tidak valid";
+}
+
+console.log(`${tanggal} ${bulan} ${tahun}`);
